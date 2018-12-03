83f0ee5f89c6e8c40cdf3057f6d9fdf0000e5d26
chikara <chikara.f.yoshida@gmail.com>
2018-12-03 18:19:53 +0900

    voice recognitionで 「お願いします」機能追加。キタコレ

--- movement_check.py
+++ movement_check.py
@@ -7,33 +7,33 @@ class Judgement:
             if dan > 1:
                 return [[suji, dan - 1, koma]]
             elif dan == 1:
-                return False
+                return []
         elif sengo == 1:
             if dan < 9:
                 return [suji, dan + 1, koma]
             elif dan == 9:
-                return False
+                return []
 
     def movelist_kyo(self, sengo, suji, dan, koma):
-        kyo_list = []
+        valid_list = []
         if sengo == 0:
             if dan != 1:
                 for i in range(dan - 1):
-                    kyo_list.append([suji, i + 1, koma])
+                    valid_list.append([suji, i + 1, koma])
             else:
-                return False
+                []
         elif sengo == 1:
             if dan != 9:
                 for i in range(9 - dan - 1):
-                    kyo_list.append([suji, dan + 1 + i, koma])
+                    valid_list.append([suji, dan + 1 + i, koma])
             else:
-                return False
-        return kyo_list
+                return []
+        return valid_list
 
     def movelist_kei(self, sengo, suji, dan, koma):
         if sengo == 0:
             if dan == 1 or dan == 2:
-                return False
+                return []
             if suji == 1:
                 return [[suji + 1, dan - 2, koma]]
             elif suji == 9:
@@ -42,7 +42,7 @@ class Judgement:
                 return [[suji - 1, dan - 2, koma], [suji + 1, dan - 2, koma]]
         elif sengo == 1:
             if dan == 8 or dan == 9:
-                return False
+                return []
             if suji == 1:
                 return [[suji + 1, dan + +2, koma]]
             elif suji == 9:
@@ -52,39 +52,126 @@ class Judgement:
 
     def movelist_gin(self, sengo, suji, dan, koma):
         if sengo == 0:
-            if suji == 1 and dan == 1:
-                return [[2, 2, '銀']]
-            elif suji == 1 and dan == 9:
-                return [[2, 8, '銀'], [1, 8, '銀']]
-            elif suji == 9 and dan == 1:
-                return [[8, 2, '銀']]
-            elif suji == 9 and dan == 9:
-                return [[9, 8, '銀'], [8, 8, '銀']]
-            else:
-                return [[suji + 1, dan - 1, '銀'], [suji, dan - 1, '銀'], [suji - 1, dan - 1, '銀'],
-                        [suji + 1, dan + 1, '銀'], [suji - 1, dan + 1, '銀']]
+            gin_list = [[suji + 1, dan - 1, koma],
+                        [suji, dan - 1, koma],
+                        [suji - 1, dan - 1, koma],
+                        [suji + 1, dan + 1, koma],
+                        [suji - 1, dan + 1, koma]]
         elif sengo == 1:
-            if suji == 1 and dan == 1:
-                return [[2, 2, '銀'], [1, 2, "銀"]]
-            elif suji == 1 and dan == 9:
-                return [[2, 8, '銀']]
-            elif suji == 9 and dan == 1:
-                return [[8, 2, '銀'], [9, 2, '銀']]
-            elif suji == 9 and dan == 9:
-                return [[8, 8, '銀']]
-            else:
-                return [[suji + 1, dan - 1, '銀'], [suji, dan + 1, '銀'], [suji - 1, dan - 1, '銀'],
-                        [suji + 1, dan + 1, '銀'], [suji - 1, dan + 1, '銀']]
+            gin_list = [[suji + 1, dan - 1, koma],
+                        [suji - 1, dan - 1, koma],
+                        [suji + 1, dan + 1, koma],
+                        [suji, dan + 1, koma],
+                        [suji - 1, dan + 1, koma]]
+
+        valid_list = []
+        for x in gin_list:
+
+            no_ten = 10 not in x
+            no_zero = 0 not in x
+
+            if no_ten and no_zero:
+                valid_list.append(x)
+
+        return valid_list
 
     def movelist_kin(self, sengo, suji, dan, koma):
         if sengo == 0:
-            kin_list = [[suji + 1, dan - 1, '金'], [suji, dan - 1, '金'], [suji - 1, dan - 1, '金'], [suji + 1, dan, '金'],
-                        [suji - 1, dan, '金'], [suji, dan + 1, '金']]
-            print(kin_list)
-            for list in kin_list:
-                if 0 in list:
-                    kin_list.remove(list)
+            kin_list = [[suji + 1, dan - 1, '金'],
+                        [suji, dan - 1, '金'],
+                        [suji - 1, dan - 1, '金'],
+                        [suji + 1, dan, '金'],
+                        [suji - 1, dan, '金'],
+                        [suji, dan + 1, '金']]
+        elif sengo == 1:
+            kin_list = [[suji, dan - 1, '金'],
+                        [suji + 1, dan, '金'],
+                        [suji - 1, dan, '金'],
+                        [suji + 1, dan + 1, '金'],
+                        [suji, dan + 1, '金'],
+                        [suji - 1, dan + 1, '金']]
+        valid_list = []
+        for x in kin_list:
+            no_ten = 10 not in x
+            no_zero = 0 not in x
+
+            if no_ten and no_zero:
+                valid_list.append(x)
+
+        return valid_list
+
         return kin_list
+
+    def movelist_KAKU(self, sengo, suji, dan, koma):
+        valid_list = []
+        kaku_suji = suji
+        kaku_dan = dan
+        while (2 <= kaku_suji <= 8) and (2 <= kaku_dan <= 8):
+            valid_list.append([kaku_suji - 1, kaku_dan - 1, koma])
+            kaku_suji = kaku_suji - 1
+            kaku_dan = kaku_dan - 1
+        kaku_suji = suji
+        kaku_dan = dan
+        while (2 <= kaku_suji <= 8) and (2 <= kaku_dan <= 8):
+            valid_list.append([kaku_suji + 1, kaku_dan - 1, koma])
+            kaku_suji = kaku_suji + 1
+            kaku_dan = kaku_dan - 1
+        kaku_suji = suji
+        kaku_dan = dan
+        while (2 <= kaku_suji <= 8) and (2 <= kaku_dan <= 8):
+            valid_list.append([kaku_suji + 1, kaku_dan + 1, koma])
+            kaku_suji = kaku_suji + 1
+            kaku_dan = kaku_dan + 1
+        kaku_suji = suji
+        kaku_dan = dan
+        while (2 <= kaku_suji <= 8) and (2 <= kaku_dan <= 8):
+            valid_list.append([kaku_suji - 1, kaku_dan + 1, koma])
+            kaku_suji = kaku_suji - 1
+            kaku_dan = kaku_dan + 1
+        return valid_list
+
+    def movelist_HISYA(self, sengo, suji, dan, koma):
+        valid_list = []
+        kaku_suji = suji
+        kaku_dan = dan
+        while (2 <= kaku_suji <= 8) and (2 <= kaku_dan <= 8):
+            valid_list.append([kaku_suji - 1, kaku_dan, koma])
+            kaku_suji = kaku_suji - 1
+        kaku_suji = suji
+        kaku_dan = dan
+        while (2 <= kaku_suji <= 8) and (2 <= kaku_dan <= 8):
+            valid_list.append([kaku_suji, kaku_dan - 1, koma])
+            kaku_dan = kaku_dan - 1
+        kaku_suji = suji
+        kaku_dan = dan
+        while (2 <= kaku_suji <= 8) and (2 <= kaku_dan <= 8):
+            valid_list.append([kaku_suji + 1, kaku_dan, koma])
+            kaku_suji = kaku_suji + 1
+        kaku_suji = suji
+        kaku_dan = dan
+        while (2 <= kaku_suji <= 8) and (2 <= kaku_dan <= 8):
+            valid_list.append([kaku_suji, kaku_dan + 1, koma])
+            kaku_dan = kaku_dan + 1
+        return valid_list
+
+    def movelist_GYOKU(self, sengo, suji, dan, koma):
+        gyoku_list = [[suji + 1, dan - 1, '玉'],
+                      [suji, dan - 1, '玉'],
+                      [suji - 1, dan - 1, '玉'],
+                      [suji + 1, dan, '玉'],
+                      [suji - 1, dan, '玉'],
+                      [suji + 1, dan + 1, "玉"],
+                      [suji, dan + 1, '玉'],
+                      [suji - 1, dan + 1, "玉"]]
+        valid_list = []
+        for x in gyoku_list:
+            no_ten = 10 not in x
+            no_zero = 0 not in x
+
+            if no_ten and no_zero:
+                valid_list.append(x)
+
+        return valid_list
 
 
 class MyTestCase(unittest.TestCase):
@@ -108,15 +195,52 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_銀の動ける範囲を返す_先後判定あり(self):
-        expected = [[2, 2, '銀']]
+        expected = [[2, 2, '銀'], [1, 2, "銀"]]
         sashite = Judgement()
-        actual = sashite.movelist_gin(0, 1, 1, "銀")
+        actual = sashite.movelist_gin(1, 1, 1, "銀")
         self.assertEqual(expected, actual)
 
     def test_金の動ける範囲を返す(self):
-        expected = [[6, 4, '金'], [5, 4, '金'], [4, 4, '金'], [6, 5, '金'], [4, 5, '金'], [5, 6, '金'], ]
+        expected = [[8, 1, '金'], [9, 2, '金'], [8, 2, '金']]
         sashite = Judgement()
-        actual = sashite.movelist_kin(0, 9, 1, "金")
+        actual = sashite.movelist_kin(1, 9, 1, "金")
+        self.assertEqual(expected, actual)
+
+    def test_角の動ける範囲を返す(self):
+        sashite = Judgement()
+        expected = [[4, 4, '角'],
+                    [3, 3, '角'],
+                    [2, 2, '角'],
+                    [1, 1, '角'],
+                    [6, 4, '角'],
+                    [7, 3, '角'],
+                    [8, 2, '角'],
+                    [9, 1, '角'],
+                    [6, 6, '角'],
+                    [7, 7, '角'],
+                    [8, 8, '角'],
+                    [9, 9, '角'],
+                    [4, 6, '角'],
+                    [3, 7, '角'],
+                    [2, 8, '角'],
+                    [1, 9, '角']]
+        actual = sashite.movelist_KAKU(0, 5, 5, "角")
+        self.assertEqual(expected, actual)
+
+    def test_飛車の動ける範囲を返す(self):
+        sashite = Judgement()
+        expected = [[4, 5, "飛"], [3, 5, "飛"], [2, 5, "飛"], [1, 5, "飛"], [5, 4, "飛"], [5, 3, "飛"], [5, 2, "飛"],
+                    [5, 1, "飛"], [6, 5, "飛"], [7, 5, "飛"], [8, 5, "飛"], [9, 5, "飛"], [5, 6, "飛"], [5, 7, "飛"],
+                    [5, 8, "飛"], [5, 9, "飛"]]
+
+        actual = sashite.movelist_HISYA(0, 5, 5, '飛')
+        self.assertEqual(expected, actual)
+
+    def test_玉の動ける範囲を返す(self):
+        sashite = Judgement()
+        expected = [[6, 4, "玉"], [5, 4, "玉"], [4, 4, "玉"], [6, 5, "玉"], [4, 5, "玉"], [6, 6, "玉"], [5, 6, "玉"],
+                    [4, 6, "玉"]]
+        actual = sashite.movelist_GYOKU(0, 5, 5, '玉')
         self.assertEqual(expected, actual)
 
 
---
+++ syogi.py
@@ -0,0 +1,100 @@
+import os
+
+import speech_recognition as sr
+
+dan0 = [[1, '香'], [1, '桂'], [1, '銀'], [1, '金'], [1, "玉"], [1, '金'], [1, '銀'], [1, '桂'], [1, '香']]
+dan1 = [[2, '＊'], [1, '飛'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [1, '角'], [2, '＊']]
+dan2 = [[1, '歩'], [1, '歩'], [1, '歩'], [1, '歩'], [1, '歩'], [1, '歩'], [1, '歩'], [1, '歩'], [1, '歩']]
+dan3 = [[2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊']]
+dan4 = [[2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊']]
+dan5 = [[2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊']]
+dan6 = [[0, '歩'], [0, '歩'], [0, '歩'], [0, '歩'], [0, '歩'], [0, '歩'], [0, '歩'], [0, '歩'], [0, '歩']]
+dan7 = [[2, '＊'], [0, '角'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [2, '＊'], [0, '飛'], [2, '＊']]
+dan8 = [[0, '香'], [0, '桂'], [0, '銀'], [0, '金'], [0, "玉"], [0, '金'], [0, '銀'], [0, '桂'], [0, '香']]
+
+shogiban = [dan0, dan1, dan2, dan3, dan4, dan5, dan6, dan7, dan8]
+mochigoma_opponent = []
+mochigoma_me = []
+
+
+def display(board):
+    for dan in range(len(board)):
+        for i in board[dan]:
+            print(i[1], end="")
+        print("")
+
+
+def main():
+    turn = 0
+    while True:
+        if (turn == 0 and len(mochigoma_me) > 0) or (turn == 1 and len(mochigoma_opponent) > 0):
+            question_drop = input("持ち駒を使いますか?(y/n)")
+            if question_drop.lower() == 'y':
+                drop = input('どの駒を使いますか?')
+                print("移動先")
+                goal_Suji = int(input("筋"))
+                goal_Dan = int(input("段"))
+                if shogiban[goal_Dan - 1][9 - goal_Suji] != [2, "＊"]:
+                    print("不正な指し手です")
+                shogiban[goal_Dan - 1][9 - goal_Suji] = [0, drop]
+                if turn == 0:
+                    num = mochigoma_me.index([0, drop])
+                    mochigoma_me.pop(num)
+                elif turn == 1:
+                    num = mochigoma_opponent.index([0, drop])
+                    mochigoma_opponent.pop(num)
+
+                if turn == 0:
+                    turn = 1
+                elif turn == 1:
+                    turn = 0
+                print(mochigoma_opponent)
+                display(shogiban)
+                print(mochigoma_me)
+                continue
+
+        Origin_Suji = int(input("筋"))
+        Origin_Dan = int(input("段"))
+        koma = shogiban[Origin_Dan - 1][9 - Origin_Suji]
+
+        if shogiban[Origin_Dan - 1][9 - Origin_Suji][0] == turn:
+            print("移動先")
+            goal_Suji = int(input("筋"))
+            goal_Dan = int(input("段"))
+            moved = shogiban[goal_Dan - 1][9 - goal_Suji]
+
+            if moved[0] != turn and moved[0] != 2:
+                moved[0] = turn
+                if turn == 0:
+                    mochigoma_me.append(moved)
+                else:
+                    mochigoma_opponent.append(moved)
+
+            shogiban[Origin_Dan - 1][9 - Origin_Suji] = [2, '＊']
+            shogiban[goal_Dan - 1][9 - goal_Suji] = koma
+
+            if turn == 0:
+                turn = 1
+            elif turn == 1:
+                turn = 0
+        else:
+            print("不正な指し手です。")
+            continue
+
+        print(mochigoma_opponent)
+        display(shogiban)
+        print(mochigoma_me)
+
+
+if __name__ == "__main__":
+    display(shogiban)
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+    text = (r.recognize_google(audio, language='ja-JP'))
+    if text == 'お願いします':
+        os.system("say 'お願いします。'")
+    main()

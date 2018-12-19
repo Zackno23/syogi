adcc32f659b09e93b44cdab4e43bb21934e3ea0e
chikara <chikara.f.yoshida@gmail.com>
2018-12-19 18:22:28 +0900

    test.py 技巧からの返事をF記法で編集できるようにした。 あとは局面をsfenに変換するプロシージャを書けば、AI対戦ができる!!

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import requests
 
+sfen = "lr5nl/2g1kg1s1/p1npppbpp/2ps5/8P/2P3R2/PP1PPPP1N/1SGB1S3/LN1KG3L w 2Pp 1"
+
 r = requests.get(
-    url='https://17xn1ovxga.execute-api.ap-northeast-1.amazonaws.com/production/gikou?byoyomi=10000&position=sfen lr5nl/2g1kg1s1/p1npppbpp/2ps5/8P/2P3R2/PP1PPPP1N/1SGB1S3/LN1KG3L w 2Pp 1')
+    url=f'https://17xn1ovxga.execute-api.ap-northeast-1.amazonaws.com/production/gikou?byoyomi=10000&position=sfen {sfen}')
 # print(r)
 # print(r.text)
 data = r.json()

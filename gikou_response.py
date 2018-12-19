7480c5788acddf90ab5a40772f8f0c9352dcb310
chikara <chikara.f.yoshida@gmail.com>
2018-12-19 18:35:28 +0900

    歩が19枚あっても技巧は手を返してきた。 一手一手評価するこの方法では、sfenでリクエスト下地店でのルール的は矛盾は虫されるみたい。 ということは入力する時点でのルールチェックをしないとおかしなことになる(2歩など)

--- gikou_response.py
+++ gikou_response.py
@@ -1,6 +1,6 @@
 import requests
 
-sfen = "lr5nl/2g1kg1s1/p1npppbpp/2ps5/8P/2P3R2/PP1PPPP1N/1SGB1S3/LN1KG3L w 2Pp 1"
+sfen = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1"
 
 r = requests.get(
     url=f'https://17xn1ovxga.execute-api.ap-northeast-1.amazonaws.com/production/gikou?byoyomi=10000&position=sfen {sfen}')

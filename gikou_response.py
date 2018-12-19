3c4e651f95e511667341a68ae50535f83ed17654
chikara <chikara.f.yoshida@gmail.com>
2018-12-19 18:44:36 +0900

    test.py sfenでは、w = 将棋盤の向こう側          b = こっち側 みたい。 局面反転(ひふみんアイ)を入れるなら気をつけるべし

--- gikou_response.py
+++ gikou_response.py
@@ -1,10 +1,13 @@
 import requests
 
-sfen = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1"
 
-r = requests.get(
-    url=f'https://17xn1ovxga.execute-api.ap-northeast-1.amazonaws.com/production/gikou?byoyomi=10000&position=sfen {sfen}')
-# print(r)
-# print(r.text)
-data = r.json()
-print(data['bestmove'])
+def gikou():
+    sfen = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1"
+    r = requests.get(
+        url=f'https://17xn1ovxga.execute-api.ap-northeast-1.amazonaws.com/production/gikou?byoyomi=10000&position=sfen {sfen}')
+    data = r.json()
+    gikou_result = data['bestmove']
+    print(gikou_result)
+
+
+gikou()

76ef1c3f244173c5785aa3eb5b911b08ed9e4453
chikara <chikara.f.yoshida@gmail.com>
2018-12-19 18:14:53 +0900

    bestmoveのパースに成功

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import requests
 
 r = requests.get(
-    url='https://17xn1ovxga.execute-api.ap-northeast-1.amazonaws.com/production/gikou?byoyomi=10000&position=sfen%20lr5nl/2g1kg1s1/p1npppbpp/2ps5/8P/2P3R2/PP1PPPP1N/1SGB1S3/LN1KG3L%20w%202Pp%201')
-print(r)
-print(r.text)
+    url='https://17xn1ovxga.execute-api.ap-northeast-1.amazonaws.com/production/gikou?byoyomi=10000&position=sfen lr5nl/2g1kg1s1/p1npppbpp/2ps5/8P/2P3R2/PP1PPPP1N/1SGB1S3/LN1KG3L%20w%202Pp%201')
+# print(r)
+# print(r.text)
+data = r.json()
+print(data['bestmove'])
